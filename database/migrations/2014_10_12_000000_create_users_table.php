3f29d24b0621e29e301f1a1acfee32dd4b416440
john-carter <chachat_2015@hotmail.com>
2020-04-24 19:35:41 +0700

    ปรับการเพิ่ม user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,15 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username');
-            $table->string('name');
-            $table->string('surname');
-            $table->string('password');
+            $table->string('username')->nullable();
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('password')->nullable();
             $table->string('email')->unique();;
             $table->string('phone')->nullable();
             $table->string('line')->nullable();
             $table->string('facebook')->nullable();
-            $table->string('role_id');
+            $table->string('role_id')->nullable();
             $table->string('role')->nullable();
             $table->string('image_url')->nullable();
             $table->timestamp('email_verified_at')->nullable();
