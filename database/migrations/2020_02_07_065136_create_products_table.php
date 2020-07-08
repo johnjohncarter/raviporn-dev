09f11f2650fbf7ce9586e9eeda7960bbcf5e894a
john-carter <chachat_2015@hotmail.com>
2020-07-08 23:06:56 +0700

    ปรับการแก้ไข้ order

--- database/migrations/2020_02_07_065136_create_products_table.php
+++ database/migrations/2020_02_07_065136_create_products_table.php
@@ -14,7 +14,7 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id')->nullable();
+            $table->bigIncrements('id');
             $table->string('name')->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
