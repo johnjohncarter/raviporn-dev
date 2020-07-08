07bd18a1225293f01f5d8a76b1cdd49ff9f2c7fb
john-carter <chachat_2015@hotmail.com>
2020-07-08 22:59:57 +0700

    ปรับการแก้ไข้ order

--- database/migrations/2020_02_07_064611_create_orders_table.php
+++ database/migrations/2020_02_07_064611_create_orders_table.php
@@ -14,7 +14,7 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->bigIncrements('id')->nullable();
+            $table->bigIncrements('id');
             $table->integer('user_id')->nullable();
             $table->dateTime('order_date')->nullable();
             $table->time('order_time')->nullable();
