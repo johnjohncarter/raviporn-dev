4d26ad5fd211716d30c9a83acf939ecd0ac9e9f7
john-carter <chachat_2015@hotmail.com>
2020-07-23 17:45:09 +0700

    ปรับการแก้ไข้ order

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -11,17 +11,22 @@ class DashboardController extends Controller
 {
     public function dashboard() {
 
-        $color = ['bg-info', 'bg-danger', 'bg-blue', 'bg-warning', ];
+        $color = ['bg-fuchsia', 'bg-purple', 'bg-danger', 'bg-blue', 'bg-warning', 'bg-info', ];
         try {
             $orders = Product::with(['order_amount' => function($query) {
                 $query->where('order_date', '>=', Carbon::today());
             }])->get();
             $products = [];
+            $index_color = 0;
             foreach ($orders as $index => $order) {
+                if ($index_color > 5)
+                    $index_color = 0;
+
                 $current_data = $order;
                 $current_data['order_amount'] = $order->order_amount->sum('amount');
-                $current_data['bg_color'] = $color[array_rand($color)];
+                $current_data['bg_color'] = $color[$index_color];
                 array_push($products, $current_data);
+                $index_color++;
             }
         } catch (\Exception $exception) {
             return view('dashboard.dashboard', ['errors' => $exception->getMessage()]);
